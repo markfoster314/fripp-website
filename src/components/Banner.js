93245b15f105925be367dbb6e53599d84a0a951f
Mark Foster <markfoster314@yahoo.com>
2020-01-24 09:15:26 -0500

    First Commit

---
+++ src/components/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Welcome to Fripp Island Properties</h1>
+            </header>
+            <div className="content">
+                <p>Book your next vacation on a magnificent private Island
+                    complete with ample beach access, golf courses,
+                    cabanas, lounges and more</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">See our properties</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
